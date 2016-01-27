543bed5c1a964290e71cf25257c7b1d96b659197
Radamés Ajna <radamajna@gmail.com>
2016-01-27 17:45:21 +0000

    sketch example understanding push and pop

---
+++ 03-functions-transform-explain/sketch.js
@@ -0,0 +1,45 @@
+//This is an empty template
+
+function setup() {
+	var myCanvas = createCanvas(500, 500);
+	myCanvas.parent('myCanvas');
+	frameRate(24);
+	
+}
+
+function draw() {
+	//backgroud(RED,GREEN,BLUE)
+  	background(255,255,255); //background color WHITE
+	drawGrid(0,0,0,255);
+}
+
+
+function drawGrid(posX, posY, angle, color){
+	var size = 10; //grid size
+	var stepX = width/size; //step for X
+	var stepY = height/size;  //step for Y
+	
+	
+	fill(255-color);
+	rect(posX, posY,55,145);
+	fill(color);
+	ellipse(posX, posY, 100,50 );
+	
+	push(); //push save the syles and the coordinates state
+		translate(posX,posY);
+		rotate(radians(angle));
+		
+		var x = 0;
+		strokeWeight(2);
+		while(x <= width){
+			line(x,0,x, height);
+			x = x + stepX;
+		}
+		var y = 0;
+		while(y <= height){
+			line(0,y, width, y);
+			y = y + stepY;
+		}		
+	pop(); // brings back the syle and the coodinates state
+
+}
