6babc2bf2c6c2bda878a7a0fd57a14cb1a9c969a
Radamés Ajna <radamajna@gmail.com>
2016-02-05 15:29:05 +0000

    variables movement boundary detection

---
+++ 08-animation-moving-around/sketch.js
@@ -0,0 +1,94 @@
+
+var enableSound = false;
+var stepX = 10;
+var stepY = 10;
+var posX = 0;
+var posY = 0;
+
+var osc;
+
+
+function setup() {
+	var myCanvas = createCanvas(windowWidth, windowHeight);
+	myCanvas.parent('myCanvas');
+	frameRate(24);
+	textFont("Helvetica");
+	textSize(20);
+	
+	
+	//crazy sound functions, 
+	osc = new p5.SinOsc(); 
+	osc.amp(.5);
+
+}
+
+function draw() {
+	posX = posX + stepX;
+	if(posX > windowWidth){
+		stepX = -stepX;
+		posX = windowWidth;
+	}
+	if(posX < 0){
+		stepX = -stepX;
+		posX = 0;
+	}
+	posY = posY + stepY;
+	if(posY > windowHeight){
+		stepY = -stepY;
+		posY = windowHeight;
+	}
+	if(posY < 0){
+		stepY = -stepY;
+		posY = 0;
+	}
+	
+  	background(255,255,255); //background color WHITE
+	drawFace(posX,posY, 0,1);
+	
+	fill(255,0,0);
+	text("posX: " + posX, 10,50);
+	text("posY: " + posY, 10,70);
+
+	if(enableSound){
+    	var freqValue = midiToFreq(countAnimation);
+    	osc.freq(freqValue);		
+	}
+	
+}
+
+
+
+function drawFace(posX, posY, angle, scaleNum){
+	push(); //push save the syles and the coordinates state
+		//head
+		translate(posX,posY);
+		scale( scaleNum );
+		rotate( radians(angle) );
+		ellipse(0, 0, 100,100);
+		//eye1
+		fill(0);
+		ellipse(12, -10, 10,10);
+		//eye2
+		ellipse(-12, -10, 10,10);
+		noFill();
+		//arc needs
+		//position, width, height, angle start, angle end
+		arc(0, 0, 50, 50, radians(30), radians(150));
+	pop(); // brings back the syle and the coodinates state
+
+}
+
+function keyTyped(){
+	if(key === 's'){// if you press space go to next patern
+		enableSound = !enableSound;
+		if(enableSound === true){
+			osc.start();
+		}else{
+			osc.stop();
+		}
+	}
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
